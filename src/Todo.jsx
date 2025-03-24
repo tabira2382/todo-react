cf3521d2f1d36cd8e9033a92fd9be6ac636cd3d5
tabira2382 <tabira2382@gmail.com>
2025-03-24 22:42:37 +0900

    incompleteTodos作成

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -39,21 +39,7 @@ export const Todo = () => {
         onChange={onChangeTodoText} 
         onClick={onClickAdd} 
       />
-      <div className="incomplete-area">
-        <p className="title">未完了のTODO</p>
-        <ul>
-          {incompleteTodos.map((todo, index) => (
-              <li key={todo}>
-                <div className="list-row">
-                  <p className="todo-item">{todo}</p>
-                  <button onClick= {() => onClickComplete(index)}>完了</button>
-                  <button onClick= {() => onClickDelete(index)}>削除</button>
-                </div>
-              </li> 
-            )
-          )}
-        </ul>
-      </div>
+      
       <div className="complete-area" >
       <ul>
         <p className="title">完了のTODO</p>
--- src/components/inputTodo.jsx
+++ src/components/inputTodo.jsx
@@ -10,4 +10,4 @@ export const InputTodo = (props) => {
         <button onClick={onClick}>追加</button>
       </div>
   );
-};  
+}; 
